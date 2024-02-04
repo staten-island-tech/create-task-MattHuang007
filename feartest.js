db9ce526e192e7725e8c9e5d235e9238875dcabb
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-02-04 18:04:44 -0500

    s

--- feartest.js
+++ feartest.js
@@ -1,4 +1,41 @@
-import "./feartest.css";
-function onclick{
+const sources = [
+  "./pictures/scar9.jfif",
+  "./pictures/scary1.jfif",
+  "./pictures/scary2.jfif",
+  "./pictures/scary3.jfif",
+  "./pictures/scary4.jfif",
+  "./pictures/scary5.jfif",
+  "./pictures/scary6.jfif",
+  "./pictures/scary8.jfif",
+  "./pictures/scary7.jfif",
+  "./pictures/scary10.jfif",
+];
 
+const button = document.getElementById("circle");
+let scarecounter = getRandomInt(50, 1);
+button.addEventListener("click", (event) => {
+  //handle click'
+  console.log(event.target.innerText);
+  const counter = parseInt(event.target.innerText) + 1;
+  event.target.innerText = counter;
+  if (counter == scarecounter) {
+    console.log("boo");
+    event.target.innerText = 0;
+    const body = document.getElementsByTagName("body")[0];
+    const image = document.createElement("img");
+    image.className = "imageclass";
+    const randomIdx = Math.floor(Math.random() * sources.length);
+    image.src = sources[randomIdx];
+    body.appendChild(image);
+    setTimeout(() => {
+      image.remove();
+    }, 2000);
+    scarecounter = getRandomInt(10, 1);
+    console.log(randomIdx, sources[randomIdx]);
+  }
+});
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// ex: range = 20, offset = 5, generates 5 to 25
+function getRandomInt(range, offset) {
+  return Math.floor(Math.random() * range) + offset;
 }
